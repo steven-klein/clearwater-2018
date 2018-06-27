d354325115eb3d7d3ce6b8c55a0c5c35456a4477
Steven Klein <steven.p.klein@gmail.com>
2018-06-27 10:41:12 -0500

    full download share link

--- resources/data/index.js
+++ resources/data/index.js
@@ -9,5 +9,5 @@ module.exports = {
     "environment": process.env.NODE_ENV || "development",
     "production": (process.env.NODE_ENV === "production"),
     "images": JSON.stringify(images.get()),
-    "download_url": false
+    "download_url": "https://drive.google.com/drive/folders/1sW1FUim8lotEZBH2jesYHJGZqM0_Qmar?usp=sharing"
 }
